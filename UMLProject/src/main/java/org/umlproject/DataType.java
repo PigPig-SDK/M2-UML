467483b89e67860b2e5b25dec5cf7f90dadfc439
AdamH27-arch <amhill_1@hotmail.com>
2025-10-08 16:44:33 -0700

    Changed DataField class, created Visibility and DataType enum classes

---
+++ UMLProject/src/main/java/org/umlproject/DataType.java
@@ -0,0 +1,12 @@
+//Enum class representing primitive data types, and OTHER for custom ones
+public enum DataType {
+    BYTE,
+    SHORT,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    CHAR,
+    BOOLEAN,
+    OTHER
+}
---
+++ UMLProject/src/main/java/org/umlproject/Visibility.java
@@ -0,0 +1,9 @@
+
+//enum to set the visibility of methods and data fields
+public enum Visibility {
+
+    PUBLIC,
+    PRIVATE,
+    PROTECTED,
+    DEFAULT
+}
