bc590cae1d5af9957a75f2432434ff00d962046d
Ty Greene <kidwhix@gmail.com>
2025-10-08 10:52:46 -0700

    began implementation of umldocument methods

--- UMLProject/src/test/java/com/unittests/UMLDocumentTests.java
+++ UMLProject/src/test/java/com/unittests/UMLDocumentTests.java
@@ -15,4 +15,16 @@ public class UMLDocumentTests
         // Assert
         assertEquals(fileString, umldocument.getFileLocation()); 
     }
+    @org.junit.jupiter.api.Test
+    public void addClass_singleclass_success()
+    {
+        // Arrange
+        UMLDocument umldocument = new UMLDocument("test.json");
+        // Act
+        umldocument.addClass("test class");
+        umldocument.addClass("test class 2!!!!!!!!");
+        // Assert
+        assertEquals(2, umldocument.getClassCount());
+    }
+
 }
