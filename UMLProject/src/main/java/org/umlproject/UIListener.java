14592b4b7ba1d059733db1ea26ec54515a117365
Sean H <DjPigPigstar@gmail.com>
2025-10-19 18:42:09 -0700

    Interfaces for UI

---
+++ UMLProject/src/main/java/org/umlproject/UIListener.java
@@ -0,0 +1,21 @@
+package org.umlproject;
+
+public interface UIListener {
+    /** 
+     * The GUI listener should catch the update and draw its required bullshit.
+     */
+    void update(UMLDiagramElement desiredElement);
+    /**
+     * Tells the GUIListener they should only update the 'selected' state.
+     */
+    void updateSelected(UMLDiagramElement desiredElement);
+    /**
+     * Tells the GUIListener to only update the 'location' of its drawn elements.
+     */
+    void updateLocation(UMLDiagramElement desiredElement);
+    /**
+     * When this is called, our object removes all leftover UI elements.
+     * Called when the bound Object is no longer valid.
+     */
+    void cleanUp();
+}
---
+++ UMLProject/src/main/java/org/umlproject/UMLSelectable.java
@@ -0,0 +1,35 @@
+package org.umlproject;
+
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+
+/**
+ * UMLPositional is for storing GUI related information within each UMLClass, UMLRelationship, and so on...
+ * 
+ * This is kept within the UMLClass because it's inherently related to the 'model' we wish to preserve with GSON.
+ * Classes that extend this will have their positional data stored in the saved JSON.
+ */
+public interface UMLSelectable {
+    /**
+     * Sets the selection status on the UMLSelectable.
+     * @param isSelected
+     */
+    void setSelected(boolean isSelected);
+    /**
+     * Gets the selection status on the UMLSelectable.
+     * @return the boolean for selectivity
+     */
+    boolean getSelected();
+    /**
+     * This is for detecting if a given point is within the 'selection' area of the element.
+     * @param selectionPoint The testing selection space
+     * @return True if the object is capable of being selected.
+     */
+    boolean contains(Point2D selectionPoint);
+    /**
+     * Checks to see if the object intersects a given selection rectangle
+     * @param selectionRectangle
+     * @return true if an intersection occurs.
+     */
+    boolean intersects(Rectangle2D selectionRectangle);
+}
