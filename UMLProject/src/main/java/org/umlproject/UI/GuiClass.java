18cd3b9007c3afd76eed9d12a4108ab54c6161a9
AdamH27-arch <amhill_1@hotmail.com>
2025-10-23 18:44:17 -0700

    worked on guiClass() constructor, and update() method
    Constructor, will create a dummy class and insert it in the scene. Dummy class will have options for modifying dataFields.

--- UMLProject/src/main/java/org/umlproject/UI/GuiClass.java
+++ UMLProject/src/main/java/org/umlproject/UI/GuiClass.java
@@ -1,19 +1,119 @@
 package org.umlproject.UI;
 
 import javafx.scene.Group;
-import org.umlproject.UIListener;
-import org.umlproject.UMLClass;
-import org.umlproject.UMLDiagramElement;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import org.umlproject.*;
+
+import java.util.Optional;
 
 public class GuiClass implements UIListener {
-    
+    private Group world;
+    private UMLClass parentClass;
+
+
     public GuiClass(Group world, UMLClass parentClass)
     {
-        //Bind our UI elements to 'world'
+        this.world = world;
+        this.parentClass = parentClass;
+
+        //create a rectangle background for the class.
+        Rectangle background = new Rectangle();
+        background.setHeight(200);
+        background.setWidth(200);
+        background.setFill(Color.LIGHTGRAY);
+        background.setStroke(Color.BLACK);
+
+        StackPane nodeBackground = new StackPane();
+        nodeBackground.getChildren().add(background);
+
+        //needs to create a node object and insert it into group
+        //gui object will consist of an outer VBox which will hold a textField for
+        //class name and two VBoxes for the fields and methods, there will be two HBoxes one that
+        //holds a dataFields: label and a addDataFields button and then another one that holds a
+        //methods: label and a addMethods button.
+        VBox guiClassNode = new VBox();
+        Text className = new Text(parentClass.getClassName());
+
+        //create data field section of gui class node. Will have label "Data Fields" and a button
+        //to add new DataFields followed by a list of current Data fields.
+        HBox dataFieldsLabelAndAdd = new HBox();
+        Label dataFields = new Label("Data Fields:");
+        Button addField = new Button("add Data Field");
+
+        //add listener to addField button that prompts user for dataField name and type
+        //then use these inputs to create a new dataField and update the umlClass
+        addField.setOnAction(e -> {
+            String newFieldTypeAndName = promptForFieldTypeAndName();
+            if(newFieldTypeAndName != null && !newFieldTypeAndName.isEmpty()){
+                String[] dataFieldTypeAndName = newFieldTypeAndName.split(" ");
+
+                String typeName = dataFieldTypeAndName[0];
+                DataType typeOfField = DataType.stringToDatatype(typeName);
+
+                String nameOfField = dataFieldTypeAndName[1];
+
+                String visibility = dataFieldTypeAndName[2];
+
+                //create dataField and add to umlClass
+                UMLDataField newField = new UMLDataField(nameOfField, typeOfField, Visibility.stringVisibility(visibility));
+                boolean success = parentClass.addField(newField);
+                if(success){
+                    System.out.println("Success Field was added!");
+                }
+                else{
+                    System.out.println("Failure! datafield was invalid or a duplicate!");
+                }
+
+            }
+        });
+        dataFieldsLabelAndAdd.getChildren().addAll(dataFields, addField);
+
+        //create methods section of gui class node. Will have label "methods" and a button to
+        //add new methods followed by a list of current methods.
+        HBox methodsLabelAndAdd = new HBox();
+        Label methods = new Label("methods:");
+        Button addMethod = new Button("add method");
+
+        //add listener to addMethod button that prompts user for method name and parameter types
+        methodsLabelAndAdd.getChildren().addAll(methods, addMethod);
+
+        VBox dataFieldList = new VBox();
+        VBox methodList = new VBox();
+
+        //add all nodes to guiClassNode and put in scene
+        guiClassNode.getChildren().addAll(className, dataFieldsLabelAndAdd,dataFieldList, methodsLabelAndAdd, methodList);
+        nodeBackground.getChildren().add(guiClassNode);
+        world.getChildren().add(nodeBackground);
     }
+
+    /**
+     * method will create a popup window within the gui after the user clicks "add Data Field" button
+     * The prompt will ask them to type in a field name
+     * @return String representing the field name.
+     */
+    private String promptForFieldTypeAndName(){
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Add Data Field name and Type and visibility");
+        dialog.setHeaderText("Enter type, name, visiblity of DataField:");
+        dialog.setContentText("Field name, type, visibility:");
+
+        Optional<String> result = dialog.showAndWait();
+        return result.orElse(null);
+    }
+
     
     @Override
     public void update(UMLDiagramElement desiredElement) {
+
+        
     }
 
     @Override
--- UMLProject/src/main/java/org/umlproject/UI/GuiController.java
+++ UMLProject/src/main/java/org/umlproject/UI/GuiController.java
@@ -115,6 +115,19 @@ public class GuiController implements UMLGuiController {
         console.setText("");
     }
     //----------------- UMLGuiController Interface -----------------
+
+    /** This method will listen for when +C is pushed inside gui. It then retrieves the
+     * UMLDocument instance and calls addClass(with the argument "newClass change name".
+     * Not, we don't have a default constructor for UMLClass().
+     *
+      */
+    @FXML
+    public void addClassButtonPushed(){
+        UMLDocument.getInstance().addClass("newClass change name");
+
+    }
+
+    //This method will bind a guiClass listener to the new umlClass
     @Override
     public void addClass(UMLClass umlClass) {
         GuiClass guiClass = new GuiClass(world, umlClass);
