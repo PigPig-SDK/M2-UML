9b625e05ebb0ab37e62ab566d431505e37bb87a0
Sean H <DjPigPigstar@gmail.com>
2025-10-27 15:14:16 -0700

    Added generic colors class
    Allows us to use common colors across our gui without making a .css file.

---
+++ UMLProject/src/main/java/org/umlproject/UI/GuiColor.java
@@ -0,0 +1,13 @@
+package org.umlproject.UI;
+
+import javafx.scene.paint.Color;
+
+/**
+ * This exists for consistency between GUI classes.
+ */
+public class GuiColor {
+    public static final Color SELECTION_COLOR = Color.rgb(255, 197, 104);
+    public static final Color GENERIC_BACKGROUND_COLOR = Color.rgb(150, 135, 114);
+    public static final Color GENERIC_LINE_COLOR = Color.rgb(255, 254, 218);
+
+}
