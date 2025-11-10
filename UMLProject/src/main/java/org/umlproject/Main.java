f6eea47f2f9ed50104ff5718354a82619be0eca3
Sean H <DjPigPigstar@gmail.com>
2025-11-10 15:24:53 -0800

    Added terminal user identification

--- UMLProject/src/main/java/org/umlproject/Main.java
+++ UMLProject/src/main/java/org/umlproject/Main.java
@@ -15,7 +15,7 @@ import org.umlproject.UI.GuiController;
 
 public class Main extends Application
 {
-
+    private static boolean isInTerminalMode = true;
     private static final String terminalLaunchString = "-terminal";
 
     public static Stage mainStage;
@@ -35,6 +35,7 @@ public class Main extends Application
         Object buttonObject = event.getSource();
         if (buttonObject == buttonGUI){
             try {
+                isInTerminalMode = false;
                 //MainScreen.fxml BINDS TO GuiClass!
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
                 Scene scene = new Scene(loader.load());
@@ -108,5 +109,9 @@ public class Main extends Application
     public void stop(){
         System.out.println("Stopping Application");
 
+    }
+    public static boolean isInTerminalMode()
+    {
+        return isInTerminalMode;
     }
 }
