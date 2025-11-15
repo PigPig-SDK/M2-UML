52f22d9eeea445424d6bc8edf0ec916680aa66cf
Sean H <DjPigPigstar@gmail.com>
2025-11-14 16:07:20 -0800

    Fixed failed tests

--- UMLProject/src/test/java/com/unittests/DocumentPacketHandlerTests.java
+++ UMLProject/src/test/java/com/unittests/DocumentPacketHandlerTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.networking.DocumentPacketHandler;
 import org.networking.NetworkPacket;
 import org.networking.PacketType;
+import org.umlproject.MainThreadDispatcher;
 import org.umlproject.UMLDocument;
 
 
@@ -16,25 +17,32 @@ public class DocumentPacketHandlerTests {
     @Test
     public void handleDocumentPacket_validPacket_updatesDocument() throws IOException {
         //Assign
+        MainThreadDispatcher.dispatcher = new MainThreadDispatcher();
         UMLDocument doc = new UMLDocument("test");
         doc.addClass("a");
         NetworkPacket packet = NetworkPacket.objectToNetworkPacket(0, PacketType.FULL_DOCUMENT, doc);
         //Act
         DocumentPacketHandler.handleDocumentPacket(packet);
+        MainThreadDispatcher.dispatcher.processQueuedActions();
         //Assert
         Assertions.assertNotNull(UMLDocument.getInstance().getClass("a"));
         UMLDocument.resetInstance();
+        
+        MainThreadDispatcher.dispatcher = null;
     }
     @Test
     public void handleDocumentPacket_invalidPacket_doesNothing() throws IOException {
         //Assign
+        MainThreadDispatcher.dispatcher = new MainThreadDispatcher();
         UMLDocument.getInstance().addClass("a");
         NetworkPacket packet = NetworkPacket.objectToNetworkPacket(0, PacketType.FULL_DOCUMENT, null);
         //Act
         DocumentPacketHandler.handleDocumentPacket(packet);
+        MainThreadDispatcher.dispatcher.processQueuedActions();
         //Assert
         Assertions.assertNotNull(UMLDocument.getInstance().getClass("a"));
         UMLDocument.resetInstance();
+        MainThreadDispatcher.dispatcher = null;
     }
     @Test
     public void handleDocumentPacket_invalidStringInPacket_doesNothing() throws IOException {
