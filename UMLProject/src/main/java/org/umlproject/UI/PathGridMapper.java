86ec9d3bf6c4268a73cd0a9d7e747909a14e7eef
AdamH27-arch <amhill_1@hotmail.com>
2025-11-21 16:45:19 -0800

    More or less finished PathGridMapper class
    Implemented getInitialPerimeterNodes() method.

--- UMLProject/src/main/java/org/umlproject/UI/PathGridMapper.java
+++ UMLProject/src/main/java/org/umlproject/UI/PathGridMapper.java
@@ -1,5 +1,6 @@
 package org.umlproject.UI;
 
+import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import org.umlproject.UMLClass;
 import org.umlproject.UMLDocument;
@@ -11,7 +12,8 @@ import java.util.Set;
 
 /**
  * This class is responsible for converting the continuous JavaFX coordinates to discrete grid corodinates and
- * vice versa. It will also be responsible for obstacle checking.
+ * vice versa. It will also be responsible for obstacle checking and generating the perimeter AStarNodes that will
+ * be used to initialize the open set for the AStar algorithm.
  */
 public class PathGridMapper {
 
@@ -50,7 +52,6 @@ public class PathGridMapper {
         return cellCenterCoord;
     }
 
-
     /**
      * This method will check to see if the AStarNode whose center is given by (gridI, gridJ) is contained
      * by any non-goal class boxes. If it is, return false, otherwise return true.
@@ -63,7 +64,7 @@ public class PathGridMapper {
         double testY = toPixelCoordinate(gridJ);
         List<GuiClass> guiClasses = GuiController.extractGuiClasses();
         for(GuiClass nextClass : guiClasses){
-            if(nextClass == targetClass.getListener()){
+            if(nextClass.getParentClass() == targetClass){
                 continue;
             }
             Rectangle2D classBounds = nextClass.getRectBounds();
@@ -81,5 +82,60 @@ public class PathGridMapper {
         return true;
     }
 
+    /**
+     * USED AI to build this Method.
+     * This method is responsible for creating AStarNodes for every tile along the perimeter of the source
+     * class box. These nodes will be used to initialize the open set so that we can begin the A star search. Pad the initial
+     * source class box bounds with grid tiles of size 1.5, then scan each tile within this padded rectangle to extract
+     * perimeter AStarNodes.
+     * @param sourceGui, starting class box
+     * @param target, target class box
+     * @return
+     */
+    public List<AStarNode> getInitialPerimeterNodes(GuiClass sourceGui, UMLClass target) {
+        List<AStarNode> perimeterNodes = new ArrayList<>();
+        Rectangle2D sourceBounds = sourceGui.getRectBounds();
+        Point2D targetCenter = target.getLocation();
+        double buffer = PathGridMapper.GRID_SIZE * 1.5;
 
+        //Define padded search area around source bounds.
+        double searchMinX = sourceBounds.getMinX() - buffer;
+        double searchMaxX = sourceBounds.getMaxX() + buffer;
+        double searchMinY = sourceBounds.getMinY() - buffer;
+        double searchMaxY = sourceBounds.getMaxY() + buffer;
+
+        //Convert continuous coordinate boundaries into discrete grid indices.
+        int minI = PathGridMapper.toGridIndex(searchMinX);
+        int maxI = PathGridMapper.toGridIndex(searchMaxX);
+        int minJ = PathGridMapper.toGridIndex(searchMinY);
+        int maxJ = PathGridMapper.toGridIndex(searchMaxY);
+
+        //Cycle through the discrete grid tile indices and check to see which tiles are along the perimeter
+        //of the class box and which tiles are at least partially inside the class box. Fill the perimeterNodes list
+        //only with those tiles along the perimeter that don't intersect the class box.
+        for(int i = minI; i <= maxI; i++){
+            for(int j = minJ; j <= maxJ; j++){
+                double testX = PathGridMapper.toPixelCoordinate(i);
+                double testY = PathGridMapper.toPixelCoordinate(j);
+
+                //Check to see if test coordinates are outside the unpadded
+                //source bounds.
+                if(sourceBounds.contains(testX, testY)){
+                    continue;
+                }
+                //Check for obstacles.
+                if(!this.isPassable(i, j, target)){
+                    continue;
+                }
+
+                //Node is valid so create AStarNode and insert into list.
+                double xValue = targetCenter.getX() - testX;
+                double yValue = targetCenter.getY() - testY;
+                double hCost = Math.sqrt(xValue * xValue + yValue * yValue);
+                AStarNode newNode = new AStarNode(i, j, 0.0, hCost, null);
+                perimeterNodes.add(newNode);
+            }
+        }
+        return perimeterNodes;
+    }
 }
