0ce9c0b51cdee29bc66e963e0dd6a16433bb51fd
Ty Greene <kidwhix@gmail.com>
2025-12-09 17:20:11 -0800

    Helpmenu display change
    Now a scrollpane

--- UMLProject/src/main/java/org/umlproject/UI/GuiHelpWindow.java
+++ UMLProject/src/main/java/org/umlproject/UI/GuiHelpWindow.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import org.umlproject.Main;
 
@@ -13,6 +14,14 @@ public class GuiHelpWindow {
     public static void showHelp()
     {
         InputStream in = Main.class.getResourceAsStream("help.txt");
-        FXDialogueFactory.createAlertWindow(Alert.AlertType.INFORMATION, "Help", null, null, FXDialogueFactory.parseTextToVBox(in)).show();
+        
+        VBox contentVBox = FXDialogueFactory.parseTextToVBox(in);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(contentVBox);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPrefHeight(600);
+        FXDialogueFactory.createAlertWindow(Alert.AlertType.INFORMATION, "Help", null, null, scrollPane).show();
+    
+        //FXDialogueFactory.createAlertWindow(Alert.AlertType.INFORMATION, "Help", null, null, FXDialogueFactory.parseTextToVBox(in)).show();
     }
 }
