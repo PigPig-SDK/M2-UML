b5fa35298d9a7671b18cd1cd3c4607a72d615455
Sean H <DjPigPigstar@gmail.com>
2025-12-11 11:34:11 -0800

    IOException changed

--- UMLProject/src/main/java/org/umlproject/AutoComplete.java
+++ UMLProject/src/main/java/org/umlproject/AutoComplete.java
@@ -1,5 +1,6 @@
 package org.umlproject;
 
+import java.io.IOException;
 import org.fusesource.jansi.AnsiConsole;
 import org.jline.reader.*;
 import org.jline.terminal.Terminal;
@@ -73,7 +74,7 @@ public class AutoComplete implements MementoListener<UMLDocument> {
                         }
                     }).build();
         }
-        catch (Exception e){
+        catch (IOException e){
             System.out.println("Error: " + e);
             return null;
         }
